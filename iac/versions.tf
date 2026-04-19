50b5a0c0aa09ed69fb339b73cfa9dfbc7cbbd800
Endre Glocker <glocker.endre@gmail.com>
2026-04-19 13:17:38 +0200

    testing terraform V4

--- iac/versions.tf
+++ iac/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "kubernetes" {
+    secret_suffix = "tfstate"
+    namespace     = "endre-cloud-based-distributed-systems-laboratory"
+    config_path   = "~/.kube/config"
+  }
+
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -15,11 +21,4 @@ terraform {
       version = "~> 1.14"
     }
   }
-
-  # Távoli state-et is használhatsz (pl. S3, GCS, vagy egy OKD ConfigMap-based
-  # backend), most a git-ignorált helyi state marad az egyszerűség kedvéért.
-  # backend "kubernetes" {
-  #   secret_suffix = "tfstate"
-  #   namespace     = "endre-cloud-based-distributed-systems-laboratory"
-  # }
 }
